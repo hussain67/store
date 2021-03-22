67c3fab6327cff73fb5f34f8459c19cdad28b1f2
hussain67 <37026797+hussain67@users.noreply.github.com>
2021-03-22 23:19:56 +0000

    Cart continue

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -8,9 +8,19 @@ import CartTotals from './CartTotals'
 import PageHero from './PageHero'
 
 const CartContent = () => {
-  const { cart } = useCartContext()
-  return (<Wrapper>
+  const { cart, clearCart } = useCartContext()
+  return (<Wrapper className="section section-center">
+    <CartColumns />
 
+    {cart.map(item => {
+      return <CartItem key={item.id} {...item} />
+    })}
+    <hr />
+    <div className="link-container">
+      <Link to="/products" className="link-btn">continue shopping</Link>
+      <button className="link-btn clear-btn" type="button" onClick={clearCart}>clear shopping cart</button>
+    </div>
+    <CartTotals />
 
   </Wrapper>
   )
